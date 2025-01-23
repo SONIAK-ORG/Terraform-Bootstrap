fbd28e25e9f2219d1c7d3c4a542eb2464412a9bc
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2025-01-23 15:12:24 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -40,3 +40,16 @@ variable "tenant_id" {
   description = "The Azure tenant ID"
   type        = string
 }
+
+
+variable "subscription_id" {
+  description = "The Azure subscription ID"
+  type        = string
+}
+
+
+
+
+
+
+
