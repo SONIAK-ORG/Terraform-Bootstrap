78aad9a727f56c4e60b9a9a1d6fbc750810adc40
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2025-01-23 15:02:33 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,53 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_user_assigned_identity" "fabric-identity" {
+  name                = "${var.project_name}-identity"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
+output "managed_identity_client_id" {
+  value = azurerm_user_assigned_identity.fabric-identity.client_id
+}
+
+output "managed_identity_principal_id" {
+  value = azurerm_user_assigned_identity.fabric-identity.principal_id
+}
+
+
+
+
+
+
+
 provider "azuredevops" {
   org_service_url       = var.org_url
   personal_access_token = var.pat_token
 }
 
-
 data "azuredevops_project" "project" {
   name = var.project_name
 }
+
+# Create the service connection
+resource "azuredevops_serviceendpoint_azurerm" "fabric-serviceendpoint" {
+  project_id            = data.azuredevops_project.project.id
+  service_endpoint_name = "${var.project_name}-service-connection"
+  description           = "Service connection for ${var.project_name}"
+  credentials {
+    serviceprincipalid  = azurerm_user_assigned_identity.fabric-identity.client_id
+    serviceprincipalkey = "" # Managed Identity does not require a key
+    tenantid            = var.tenant_id
+  }
+  azurerm_spn_role_assignment {
+    spn_object_id = azurerm_user_assigned_identity.fabric-identity.principal_id
+    role_id       = "b24988ac-6180-42a0-ab88-20f7382dd24c" # Contributor role
+  }
+}
+
+
 
 resource "azuredevops_git_repository" "target_repo" {
   project_id = data.azuredevops_project.project.id
@@ -15,6 +56,6 @@ resource "azuredevops_git_repository" "target_repo" {
     init_type             = "Import"
     source_type           = "Git"
     source_url            = "https://github.com/SONIAK-ORG/fabric-setup"
-    #service_connection_id = azuredevops_serviceendpoint_generic_git.example-serviceendpoint.id
+    service_connection_id = azuredevops_serviceendpoint_azurerm.fabric-serviceendpoint.id
   }
 }
