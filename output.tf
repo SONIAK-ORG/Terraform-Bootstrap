376379e955232cfe44d5c8ce88c5af38063c22a5
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2025-01-24 21:29:02 +0000

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,17 @@
+output "repository_id" {
+  description = "The ID of the created Git repository"
+  value       = azuredevops_git_repository.repo_fabric.id
+}
+
+output "repository_url" {
+  description = "The URL of the created Git repository"
+  value       = azuredevops_git_repository.repo_fabric.web_url
+}
+
+output "managed_identity_client_id" {
+  value = azurerm_user_assigned_identity.mi_fabric.client_id
+}
+
+output "managed_identity_principal_id" {
+  value = azurerm_user_assigned_identity.mi_fabric.principal_id
+}
