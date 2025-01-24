a10b53627a743a54f53d2c38c38ddb3fcbc9530e
Raphael-kainos <143803576+Raphael-kainos@users.noreply.github.com>
2025-01-24 23:23:23 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azuredevops = {
       source  = "microsoft/azuredevops"
-      version = "~> 0.4.0"
+      version = "~> 1.6.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
